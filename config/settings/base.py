f078e0d6461b6d3f5ee01d10967aa634206273f6
HoangBD <buidinhhoang1910@gmail.com>
2026-05-25 11:03:46 +0700

    fix: update storage config for Django 4.2+ compatibility

--- config/settings/base.py
+++ config/settings/base.py
@@ -105,7 +105,15 @@ CLOUDINARY_STORAGE = {
     'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
     'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
 }
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
+STORAGES = {
+    "default": {
+        "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
 
 # Resend API
 RESEND_API_KEY = os.environ.get('RESEND_API_KEY')
